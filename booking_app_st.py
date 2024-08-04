c4024b13e7f0ec97a893185321d8f18348aff160
oskari-o <oskari.ojavuo@gmail.com>
2024-08-04 18:43:03 +0300

    Fix file name

--- booking_app_st.py
+++ booking_app_st.py
@@ -1,7 +1,7 @@
 import streamlit as st
   
 pages = [
-  st.Page("Booking_app_admin.py", title="Admin", url_path="admin"),
+  st.Page("booking_app_admin.py", title="Admin", url_path="admin"),
   st.Page("booking_page.py", title="Booking", url_path="booking"),
 ]
 
